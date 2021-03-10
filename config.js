f12ae28486b3a8099ea593aa98c90d013e79addd
sim-eunji <kingeunji12@gmail.com>
2021-03-11 04:14:37 +0900

    first commit

---
+++ config.js
@@ -0,0 +1,50 @@
+'use strict'
+
+module.exports = {
+  url: 'https://lumen.netlify.com',
+  pathPrefix: '/',
+  title: '킹은지 개발 블로그',
+  subtitle: '코린이 개발 일기',
+  copyright: '© All rights reserved.',
+  disqusShortname: '',
+  postsPerPage: 4,
+  googleAnalyticsId: 'UA-73379983-2',
+  useKatex: false,
+  menu: [
+    {
+      label: 'Articles',
+      path: '/',
+    },
+    {
+      label: 'About me',
+      path: '/pages/about',
+    },
+    {
+      label: 'Contact me',
+      path: '/pages/contacts',
+    },
+  ],
+  author: {
+    name: 'kingeunji-dev',
+    photo: '/photo.jpg',
+    bio: '매일매일 느끼고 공부한 것을 기록해요 초보 프론트엔드 개발자',
+    contacts: {
+      email: '',
+      facebook: '#',
+      telegram: '#',
+      twitter: '#',
+      github: '#',
+      rss: '',
+      vkontakte: '',
+      linkedin: '#',
+      instagram: '#',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+      medium: '',
+    },
+  },
+}
